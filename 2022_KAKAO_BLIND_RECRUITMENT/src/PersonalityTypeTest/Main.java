f92c19f8d5e97b5d8edb6db3ec26b2b07a671c1a
42bahn <bbu0704@gmail.com>
2022-09-18 21:47:08 +0900

    [성격 유형 검사하기] ✅

--- 2022_KAKAO_BLIND_RECRUITMENT/src/PersonalityTypeTest/Main.java
+++ 2022_KAKAO_BLIND_RECRUITMENT/src/PersonalityTypeTest/Main.java
@@ -3,54 +3,50 @@ package PersonalityTypeTest;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-class Indicators {
-    private String type;
-    private int score;
-
-    public Indicators(String type, int score) {
-        this.type = type;
-        this.score = score;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-}
-
 class Solution {
     public String solution(String[] survey, int[] choices) {
 
-        LinkedHashMap<String, Integer> indicators = new LinkedHashMap<>();
-//        Set<Indicators> indicators = new HashSet<>();
+        String answer;
 
-        AtomicInteger index = new AtomicInteger();
-        Arrays.stream(survey).forEach(sv -> {
-            if (choices[index.get()] == 4)
-                indicators.put(sv, 0);
-            else if (choices[index.get()] < 4)
-                indicators.put(sv, -choices[index.getAndIncrement()]);
-            else
-                indicators.put(sv, choices[index.getAndIncrement()]);
+        HashMap<String, Integer> indicators = new HashMap<>();
+        indicators.put("RT", 0);
+        indicators.put("CF", 0);
+        indicators.put("JM", 0);
+        indicators.put("AN", 0);
+
+        indicators.forEach((type, score) -> {
+
+            AtomicInteger index = new AtomicInteger();
+            Arrays.stream(survey).forEach(s -> {
+                if (survey[index.get()].matches("(.*)[" + type + "]")) {
+//                    System.out.println(survey[index.get()]);
+
+                    int personalityScore = 0;
+                    if (choices[index.get()] > 4)
+                        personalityScore = survey[index.get()].equals(type) ? (choices[index.get()] % 4) : -(choices[index.get()] % 4);
+                    else if (choices[index.get()] < 4)
+                        personalityScore = survey[index.get()].equals(type) ? -(4 - choices[index.get()]) : (4 - choices[index.get()]);
+
+                    indicators.put(type, indicators.get(type) + personalityScore);
+
+                }
+                index.getAndIncrement();
+            });
+//            System.out.println(indicators.get(type));
+
         });
 
-        indicators.forEach((s, integer) -> {
-            System.out.println(s + ": " + integer);
-        });
+//        System.out.println(indicators);
 
-        String answer = "";
+        answer = String.format("%c%c%c%c",
+                indicators.get("RT") <= 0 ? 'R' : 'T',
+                indicators.get("CF") <= 0 ? 'C' : 'F',
+                indicators.get("JM") <= 0 ? 'J' : 'M',
+                indicators.get("AN") <= 0 ? 'A' : 'N'
+        );
+
+//        System.out.println(answer);
+
         return answer;
     }
 }
@@ -60,5 +56,6 @@ public class Main {
         Solution solutionClass = new Solution();
 
         solutionClass.solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5});
+        solutionClass.solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3});
     }
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# 코딩 테스트 연습
+
+## [프로그래머스 코딩 테스트 연습](https://school.programmers.co.kr/learn/challenges?order=recent&page=1)
+
+### 2022 KAKAO TECH INTERNSHIP
+  - [신고 결과 받기](https://school.programmers.co.kr/learn/courses/30/lessons/92334) ❌
+  - [두 큐 합 같게 만들기](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 🚧
+
+### 2022 KAKAO BLIND RECRUITMENT
+  - [성격 유형 검사하기](https://school.programmers.co.kr/learn/courses/30/lessons/118666) ✅
